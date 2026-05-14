fcde788243b927e7f6446bd8e00f60153ba59ffb
Muaj60 <m.studyboy49@gmail.com>
2026-05-14 15:04:25 +0600

    Better Auth Connected and MongoDb setup

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,85 @@
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterPage = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogInFunc = (data) => {
+    console.log(data);
+  };
+  console.log(errors,'errors');
+  return (
+    <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100">
+      <div className="p-4 rounded-xl bg-white">
+        <h2 className="font-bold text-3xl text-center mb-6">
+          Register Your Account
+        </h2>
+
+        <form className="space-y-4" onSubmit={handleSubmit(handleLogInFunc)}>
+             <fieldset className="fieldset">
+            <legend className="fieldset-legend">Name</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type here name"
+              {...register("name", {
+                required: "Name field is required",
+              })}
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Photo URL</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type here photo url"
+              {...register("photo", {
+                required: "Photo URL field is required",
+              })}
+            />
+            {errors.photo && (
+              <p className="text-red-500">{errors.photo.message}</p>
+            )}
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Type here email"
+              {...register("email",{ required: "Email filed must required" })}
+            />
+             {errors.password&&<p className="text-blue-500">{errors.email.message}</p>}
+          </fieldset>
+          <fieldset className="fieldset relative">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Type here password"
+              {...register("password", { required: "password filed must required" })}
+            />
+            {errors.password&&<p className="text-red-500">{errors.password.message}</p>}
+          </fieldset>
+
+          <button className="btn w-full bg-slate-800 text-white">Register</button>
+        </form>
+
+       
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
